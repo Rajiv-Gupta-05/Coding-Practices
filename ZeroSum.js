47107dbb84b05c157f2f55975d1930c3ac9a6a5e
Rajiv Gupta <rajiv.gupta1.contractor@pepsico.com>
2024-04-03 12:10:47 +0530

    Problem ZeroSum solution

---
+++ ZeroSum.js
@@ -0,0 +1,24 @@
+// Checking sum zero problem
+// [-5,-4,-3,-2,0,1,2,4,6,8] -> Input
+// [-4,4] -> Output
+
+function Zerosum(array) {
+    let left = 0;
+    let right = array.length - 1;
+  
+    while (left < right) {
+      sum = array[left] + array[right];
+      if (sum === 0) {
+        return [array[left], array[right]];
+      }
+      if (sum > 0) {
+        right--;
+      } else {
+        left++;
+      }
+    }
+  }
+  let result = Zerosum([-5, -4, -3, -2, 0, 1, 2, 4, 6, 8]);
+  console.log(result);
+  
+  // Time Complexity o(n) which is linear.
